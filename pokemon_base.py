d832a9a94e2598488f216ee052d80d19017cf789
bukkz <163957184+bukkz@users.noreply.github.com>
2024-04-14 21:52:54 +1000

    pokemon_base.py

--- pokemon_base.py
+++ pokemon_base.py
@@ -1,14 +1,7 @@
-"""
-This module contains PokeType, TypeEffectiveness and an abstract version of the Pokemon Class
-"""
 from abc import ABC
 from enum import Enum
-from data_structures.referential_array import ArrayR
 
 class PokeType(Enum):
-    """
-    This class contains all the different types that a Pokemon could belong to
-    """
     FIRE = 0
     WATER = 1
     GRASS = 2
@@ -26,183 +19,91 @@ class PokeType(Enum):
     ROCK = 14
 
 class TypeEffectiveness:
-    """
-    Represents the type effectiveness of one Pokemon type against another.
-    """
+    # matrix of effectiveness values from the CSV data provided
+    EFFECT_TABLE = (
+        (1.0, 0.5, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 1.0, 1.0, 1.0, 2.0),  # FIRE
+        (2.0, 0.5, 0.5, 1.0, 0.5, 1.0, 1.0, 1.0, 1.0, 2.0, 0.5, 1.0, 1.0, 1.0, 1.0),  # WATER
+        (0.5, 2.0, 0.5, 0.5, 0.5, 1.0, 1.0, 0.5, 1.0, 2.0, 1.0, 1.0, 0.5, 1.0, 2.0),  # GRASS
+        (2.0, 1.0, 2.0, 1.0, 1.0, 1.0, 0.5, 0.5, 0.5, 1.0, 1.0, 1.0, 2.0, 2.0, 2.0),  # BUG
+        (1.0, 1.0, 1.0, 1.0, 2.0, 0.5, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0),  # DRAGON
+        (1.0, 2.0, 0.5, 1.0, 0.5, 0.5, 1.0, 2.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0),  # ELECTRIC
+        (1.0, 1.0, 1.0, 0.5, 1.0, 1.0, 1.0, 0.5, 0.0, 1.0, 2.0, 2.0, 0.5, 0.5, 2.0),  # FIGHTING
+        (1.0, 1.0, 2.0, 2.0, 1.0, 0.5, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5),  # FLYING
+        (1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 1.0, 1.0, 0.0, 0.5, 2.0, 1.0),  # GHOST
+        (2.0, 1.0, 0.5, 0.5, 1.0, 2.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 2.0, 1.0, 2.0),  # GROUND
+        (1.0, 0.5, 2.0, 1.0, 1.0, 1.0, 0.5, 2.0, 1.0, 2.0, 0.5, 1.0, 1.0, 1.0, 1.0),  # ICE
+        (1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0),  # NORMAL
+        (1.0, 1.0, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5, 0.5, 1.0, 1.0, 0.5, 1.0, 0.5),  # POISON
+        (1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0, 0.5, 1.0),  # PSYCHIC
+        (2.0, 1.0, 1.0, 2.0, 1.0, 1.0, 0.5, 2.0, 1.0, 0.5, 2.0, 1.0, 1.0, 1.0, 1.0)   # ROCK
+    )
 
     @classmethod
     def get_effectiveness(cls, attack_type: PokeType, defend_type: PokeType) -> float:
-        """
-        Returns the effectiveness of one Pokemon type against another, as a float.
+        return cls.EFFECT_TABLE[attack_type.value][defend_type.value]
 
-        Parameters:
-            attack_type (PokeType): The type of the attacking Pokemon.
-            defend_type (PokeType): The type of the defending Pokemon.
-
-        Returns:
-            float: The effectiveness of the attack, as a float value between 0 and 4.
-        """
-        raise NotImplementedError
-
-    def __len__(self) -> int:
-        """
-        Returns the number of types of Pokemon
-        """
-        raise NotImplementedError
-
-
-class Pokemon(ABC): # pylint: disable=too-few-public-methods, too-many-instance-attributes
-    """
-    Represents a base Pokemon class with properties and methods common to all Pokemon.
-    """
-    def __init__(self):
-        """
-        Initializes a new instance of the Pokemon class.
-        """
-        self.health = None
-        self.level = None
-        self.poketype = None
-        self.battle_power = None
-        self.evolution_line = None
-        self.name = None
-        self.experience = None
-        self.defence = None
-        self.speed = None
+class Pokemon(ABC):
+    def __init__(self, name, level, poketype, battle_power, health, defence, speed, experience, evolution_line):
+        self.health = health
+        self.level = level
+        self.poketype = poketype
+        self.battle_power = battle_power
+        self.evolution_line = evolution_line
+        self.name = name
+        self.experience = experience
+        self.defence = defence
+        self.speed = speed
 
     def get_name(self) -> str:
-        """
-        Returns the name of the Pokemon.
-
-        Returns:
-            str: The name of the Pokemon.
-        """
         return self.name
 
     def get_health(self) -> int:
-        """
-        Returns the current health of the Pokemon.
-
-        Returns:
-            int: The current health of the Pokemon.
-        """
         return self.health
 
     def get_level(self) -> int:
-        """
-        Returns the current level of the Pokemon.
-
-        Returns:
-            int: The current level of the Pokemon.
-        """
         return self.level
 
     def get_speed(self) -> int:
-        """
-        Returns the current speed of the Pokemon.
-
-        Returns:
-            int: The current speed of the Pokemon.
-        """
         return self.speed
 
     def get_experience(self) -> int:
-        """
-        Returns the current experience of the Pokemon.
-
-        Returns:
-            int: The current experience of the Pokemon.
-        """
         return self.experience
 
     def get_poketype(self) -> PokeType:
-        """
-        Returns the type of the Pokemon.
-
-        Returns:
-            PokeType: The type of the Pokemon.
-        """
         return self.poketype
 
     def get_defence(self) -> int:
-        """
-        Returns the defence of the Pokemon.
-
-        Returns:
-            int: The defence of the Pokemon.
-        """
         return self.defence
 
     def get_evolution(self):
-        """
-        Returns the evolution line of the Pokemon.
-
-        Returns:
-            list: The evolution of the Pokemon.
-        """
         return self.evolution_line
 
     def get_battle_power(self) -> int:
-        """
-        Returns the battle power of the Pokemon.
-
-        Returns:
-            int: The battle power of the Pokemon.
-        """
         return self.battle_power
 
     def attack(self, other_pokemon) -> float:
-        """
-        Calculates and returns the damage that this Pokemon inflicts on the
-        other Pokemon during an attack.
-
-        Args:
-            other_pokemon (Pokemon): The Pokemon that this Pokemon is attacking.
-
-        Returns:
-            int: The damage that this Pokemon inflicts on the other Pokemon during an attack.
-        """
-        raise NotImplementedError
+        effectiveness = TypeEffectiveness.get_effectiveness(self.poketype, other_pokemon.poketype)
+        return self.battle_power * effectiveness
 
     def defend(self, damage: int) -> None:
-        """
-        Reduces the health of the Pokemon by the given amount of damage, after taking
-        the Pokemon's defence into account.
-
-        Args:
-            damage (int): The amount of damage to be inflicted on the Pokemon.
-        """
         effective_damage = damage/2 if damage < self.get_defence() else damage
         self.health = self.health - effective_damage
 
-    def level_up(self) -> None:
-        """
-        Increases the level of the Pokemon by 1, and evolves the Pokemon if it has
-          reached the level required for evolution.
-        """
+    def level_up(self):
         self.level += 1
-        if len(self.evolution_line) > 0 and self.evolution_line.index\
-            (self.name) != len(self.evolution_line)-1:
+        if len(self.evolution_line) > 1 and self.evolution_line.index(self.name) < len(self.evolution_line) - 1:
             self._evolve()
 
-    def _evolve(self) -> None:
-        """
-        Evolves the Pokemon to the next stage in its evolution line, and updates
-          its attributes accordingly.
-        """
-        raise NotImplementedError
+    def _evolve(self):
+        next_index = self.evolution_line.index(self.name) + 1
+        self.name = self.evolution_line[next_index]
+        self.health = int(self.health * 1.5)
+        self.battle_power = int(self.battle_power * 1.5)
+        self.speed = int(self.speed * 1.5)
+        self.defence = int(self.defence * 1.5)
 
     def is_alive(self) -> bool:
-        """
-        Checks if the Pokemon is still alive (i.e. has positive health).
-
-        Returns:
-            bool: True if the Pokemon is still alive, False otherwise.
-        """
         return self.get_health() > 0
 
     def __str__(self):
-        """
-        Return a string representation of the Pokemon instance in the format:
-        <name> (Level <level>) with <health> health and <experience> experience
-        """
         return f"{self.name} (Level {self.level}) with {self.get_health()} health and {self.get_experience()} experience"
