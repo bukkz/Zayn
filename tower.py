278e403d49056a1d01bbf1cf15a6b245ea8be91b
bukkz <163957184+bukkz@users.noreply.github.com>
2024-04-14 22:24:27 +1000

    Update tower.py

--- tower.py
+++ tower.py
@@ -12,18 +12,64 @@ class BattleTower:
     def __init__(self) -> None:
         raise NotImplementedError
 
-    # Hint: use random.randint() for randomisation
+    def __init__(self):
+        self.player_trainer = None
+        self.enemy_trainers = CircularQueue()  # Assuming we have a CircularQueue for holding trainers
+        self.enemy_lives = {}  # This should ideally be an ADT but explained here with dict for simplicity
+        self.enemies_defeated = 0
+
     def set_my_trainer(self, trainer: Trainer) -> None:
-        raise NotImplementedError
+        self.player_trainer = trainer
+        # Randomize player lives within the given range
+        self.player_lives = random.randint(self.MIN_LIVES, self.MAX_LIVES)
 
     def generate_enemy_trainers(self, num_teams: int) -> None:
-        raise NotImplementedError
+        for _ in range(num_teams):
+            enemy_trainer = Trainer("Enemy_" + str(_))
+            enemy_trainer.pick_team("random")  # Assuming this method sets the team in RANDOM mode
+            lives = random.randint(self.MIN_LIVES, self.MAX_LIVES)
+            self.enemy_trainers.enqueue(enemy_trainer)
+            self.enemy_lives[enemy_trainer] = lives  # Storing lives of each trainer
 
     def battles_remaining(self) -> bool:
-        raise NotImplementedError
+        if self.player_lives <= 0 or all(lives <= 0 for lives in self.enemy_lives.values()):
+            return False
+        return True
 
-    def next_battle(self) -> Tuple[Trainer, Trainer, Trainer, int, int]:
-        raise NotImplementedError
+    def next_battle(self) -> Tuple[str, Trainer, Trainer, int, int]:
+        if not self.battles_remaining():
+            return ("No more battles", self.player_trainer, None, self.player_lives, 0)
+
+        current_enemy = self.enemy_trainers.dequeue()
+        if self.enemy_lives[current_enemy] > 0:
+            # Regenerate teams
+            self.player_trainer.team.regenerate_team(BattleMode.ROTATE)
+            current_enemy.team.regenerate_team(BattleMode.ROTATE)
+
+            # Conduct battle
+            battle = Battle(self.player_trainer, current_enemy, BattleMode.ROTATE)
+            winner = battle.commence_battle()
+
+            # Process results
+            if winner is None:
+                self.player_lives -= 1
+                self.enemy_lives[current_enemy] -= 1
+                result = "Draw"
+            elif winner == self.player_trainer:
+                self.enemy_lives[current_enemy] -= 1
+                self.enemies_defeated += 1
+                result = "Player wins"
+            else:
+                self.player_lives -= 1
+                result = "Enemy wins"
+
+            # Queue the enemy for another round if they still have lives
+            if self.enemy_lives[current_enemy] > 0:
+                self.enemy_trainers.enqueue(current_enemy)
+
+            return (result, self.player_trainer, current_enemy, self.player_lives, self.enemy_lives[current_enemy])
+
+        return ("Enemy defeated", self.player_trainer, current_enemy, self.player_lives, 0)
 
     def enemies_defeated(self) -> int:
-        raise NotImplementedError
+        return self.enemies_defeated
